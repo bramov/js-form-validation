4feda61f7d7914f6dfb73f66d888246dc3b97cff
Abramov <alexey.abramov96@gmail.com>
2019-12-02 16:12:26 +0300

    v1.1 - fixing some bugs and rewriting code with .toggle method

--- script.js
+++ script.js
@@ -7,50 +7,48 @@ function validateForm(obj) {
         el.addEventListener('blur', validateInput, true);
         el.addEventListener('focus', removeError, true)
     }));
+    let submitBtn = document.querySelector('button');
+    submitBtn.addEventListener('click', submitFields);
+    let form = document.getElementById(obj.formId);
+
 
     function removeError(){
         if (this.classList.contains(obj.inputErrorClass)){
             this.classList.remove(obj.inputErrorClass);
         }
     }
-
+    //функцию ниже можно заменить на classList.toggle(obj.inputErrorClass, условие)
     function validateInput() {
         switch (this.getAttribute('data-validator')){
             case 'letters':
-                if (!this.value.match(/[a-zA-Z]+|[а-яА-я]+/g)) {
-                    this.classList.add(obj.inputErrorClass);
-                } else {
-                    this.classList.remove(obj.inputErrorClass);
-                }
+                this.classList.toggle(obj.inputErrorClass, !this.value.match(/[a-zA-Z]+|[а-яА-я]+/g));
                 break;
             case 'number':
                 if (this.getAttribute('data-validator-min') &&
                     this.getAttribute('data-validator-max')){
-                     if (this.value >= this.getAttribute('data-validator-min') &&
-                         this.value <= this.getAttribute('data-validator-max')){
-                         this.classList.remove(obj.inputErrorClass);
-                     } else {
-                         this.classList.add(obj.inputErrorClass);
-                     }
+                     this.classList.toggle(obj.inputErrorClass, (Number(this.value) < this.getAttribute('data-validator-min') ||
+                                                                 Number(this.value) > this.getAttribute('data-validator-max')));
                 } else {
-                    if (!this.value.match(/\d/g)){
-                        this.classList.add(obj.inputErrorClass);
-                    } else {
-                        this.classList.remove(obj.inputErrorClass);
-                    }
+                    this.classList.toggle(obj.inputErrorClass, !this.value.match(/^\d+$/g));
                 }
                 break;
             case 'regexp':
-                if (!this.value.match(this.getAttribute('data-validator-pattern'))){
-                    this.classList.add(obj.inputErrorClass);
-                } else {
-                    this.classList.remove(obj.inputErrorClass);
-                }
+                this.classList.toggle(obj.inputErrorClass, !this.value.match(this.getAttribute('data-validator-pattern')));
                 break;
         }
     }
 
-    //inputs[2].addEventListener('blur', checkRegEx);
+    function submitFields(e){
+        e.preventDefault();
+        const checkInputs = elem => elem.classList.contains(obj.inputErrorClass);
+        const checkEmpty = elem => elem.value.length == 0;
+        let form = document.getElementById(obj.formId);
+        let condition1 = inputs.some(checkInputs) || inputs.some(checkEmpty);
+        form.classList.toggle(obj.formInvalidClass, condition1);
+
+
+
+    }
 
 
 
